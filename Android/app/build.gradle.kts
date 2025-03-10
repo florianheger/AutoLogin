1f1e6b91c7df6eebdec3d4f4626138db5a3db7bc
Florian Heger <florian.heger@outlook.de>
2025-03-10 17:48:53 +0100

    add WorkManager

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation (libs.androidx.datastore.preferences)
     implementation(libs.okhttp)
+    implementation(libs.androidx.work.runtime.ktx)
+
 }
