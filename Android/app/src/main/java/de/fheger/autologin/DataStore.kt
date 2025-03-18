83f218d9f45d7ed6bf4b1ac4bb325859492d68ca
Florian Heger <florian.heger@outlook.de>
2025-03-18 18:44:00 +0100

    rename LoginId to DataStore; add flag for automatic login

--- Android/app/src/main/java/de/fheger/autologin/DataStore.kt
+++ Android/app/src/main/java/de/fheger/autologin/DataStore.kt
@@ -1,6 +1,7 @@
 package de.fheger.autologin
 
 import android.content.Context
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
@@ -9,10 +10,11 @@ import kotlinx.coroutines.flow.map
 
 private val Context.dataStore by preferencesDataStore(name = "login_id")
 
-class LoginId(private val context: Context) {
+class DataStore(private val context: Context) {
     companion object {
         val EMAIL_KEY = stringPreferencesKey("email")
         val PASSWORD_KEY = stringPreferencesKey("password")
+        val AUTOMATIC_LOGIN_ACTIVE_KEY = booleanPreferencesKey("automatic_login_active")
     }
 
     suspend fun saveCredentials(email: String, password: String) {
@@ -28,5 +30,15 @@ class LoginId(private val context: Context) {
 
     suspend fun getPassword(): String? {
         return context.dataStore.data.map { it[PASSWORD_KEY] }.first()
+    }
+
+    suspend fun getAutomaticLoginActive(): Boolean {
+        return context.dataStore.data.map { it[AUTOMATIC_LOGIN_ACTIVE_KEY] }.first() ?: false;
+    }
+
+    suspend fun setAutomaticLoginActive(isAutomaticLoginActive: Boolean) {
+        context.dataStore.edit { preferences ->
+            preferences[AUTOMATIC_LOGIN_ACTIVE_KEY] = isAutomaticLoginActive
+        }
     }
 }
