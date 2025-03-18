398d075c0acbc1a006f742f755fb4bf0963f3563
Florian Heger <florian.heger@outlook.de>
2025-03-18 16:21:04 +0100

    remove unused notification methods

--- Android/app/src/main/java/de/fheger/autologin/services/AutoLoginBroadcastReceiverService.kt
+++ Android/app/src/main/java/de/fheger/autologin/services/AutoLoginBroadcastReceiverService.kt
@@ -18,29 +18,6 @@ class AutoLoginBroadcastReceiverService() : BroadcastReceiver() {
                 loginId.getPassword()!!
             )
             print(message)
-            makeNotification(context, message)
         }
-    }
-
-    private fun makeNotification(context: Context, message: String) {
-//        var title = "Auto Login"
-//        var builder = NotificationCompat.Builder(context, "todo")
-//            .setContentTitle(title)
-//            .setContentText(message)
-//            .setPriority(NotificationCompat.PRIORITY_LOW)
-//        notif
-    }
-
-    private fun requestNotificationPermission(context: Context) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-//            if (ContextCompat.checkSelfPermission(context, Manifest.permission)
-//                != PackageManager.PERMISSION_GRANTED) {
-//
-//                ActivityCompat.requestPermissions(
-//                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
-//                    101
-//                )
-//            }
-//        }
     }
 }
